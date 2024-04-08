aae9ae39a52fb191ab4d09dc8cf27d4ff535dc84
D3centX <109889758+DiegosonTech@users.noreply.github.com>
2024-04-08 11:37:36 +0200

    vorterx

---
+++ plugins/vcard_owner.js
@@ -0,0 +1,42 @@
+const config = require('../../config.js');
+const { nvbuffe } = require('../../lib/assets/client.js');
+
+exports.default = {
+  name: 'owner',
+  category: 'misc',
+  carryOut: async (nova, m, { react }) => {
+    
+    await react('👤');
+
+    const { MODS, OWNER_NAME } = process;
+    const cell = MODS;
+    const ownerName = OWNER_NAME;
+    const nvlogo = "https://i.ibb.co/mCv3k97/original-e792fa6c963f76bc381b82ae539e68cc.png";
+    const sourceUrl = `https://wa.me/${cell}`;
+
+    const vcardData = `BEGIN:VCARD\nVERSION:3.0\nFN:${ownerName}\nORG:;\nTEL;type=CELL;type=VOICE;waid=${cell}:+${cell}\nEND:VCARD`;
+
+    const nvthumb = await nvbuffe(nvlogo);
+
+    const buttonMessage = {
+      contacts: {
+        displayName: ownerName,
+        contacts: [{ vcard: vcardData }],
+      },
+      contextInfo: {
+        forwardingScore: 999,
+        isForwarded: true,
+        externalAdReply: {
+          title: config.CAPTION,
+          body: m.pushName,
+          thumbnail: nvthumb,
+          mediaType: 1,
+          mediaUrl: '',
+          sourceUrl,
+        },
+      },
+    };
+
+    await nova.sendMessage(m.chat, buttonMessage, { quoted: m });
+  }
+};
