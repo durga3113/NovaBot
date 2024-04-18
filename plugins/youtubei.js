b9b190aefca34c7c337a632a150ddd516eeb459a
D3centX <109889758+Diegoson@users.noreply.github.com>
2024-04-18 16:56:29 +0200

    vorterx

--- plugins/youtubei.js
+++ plugins/youtubei.js
@@ -1,15 +1,45 @@
-const A = require('axios');
+const axios = require('axios');
 
 exports.default = {
   name: 'song',
   alias: ['audio'],
   category: 'downloads',
-  carryOut: async (nova,m,{react,args}) => {
-
-  if(!args) {
-      await react('❌');
-    return m.reply(`Provide a name: song${args}`);
-  }
-    var song = A.get(`https://api-viper-x.koyeb.app/api/song?name=${args}`);
-    const archive = res.data;
+  carryOut: async (nova, m, { react, args }) => {
     
+    try {
+      if (!args) {
+        await react('❌');
+        return m.reply('_Please provide the name of the song_');
+      }
+
+      const query = encodeURIComponent(args);
+      const res = await axios.get(`https://api-viper-x.koyeb.app/api/song?name=${query}`);
+
+      if (res.status !== 200) {
+        m.reply('NovaBot ' + re.status);
+      }
+
+       const isData = res.data;
+      if (!isData || !isData.downloadUrl) {
+        m.reply('_No song found_');
+      }
+      
+      const nvBuff = await axios.get(isData.downloadUrl, { responseType: 'arraybuffer' });
+      if (nvBuff.status !== 200) {
+        m.reply('NovaBot' + nvBuff.status);
+      }
+
+     await nova.sendMessage(m.chat, {
+        audio: nvBuff.data,
+        mimetype: 'audio/mpeg'
+      });
+
+       await react('✅');
+    } catch (error) {
+      console.error('Oops:', error);
+
+     await react('❌');
+     return m.reply('_An error occurred_');
+    }
+  }
+};
