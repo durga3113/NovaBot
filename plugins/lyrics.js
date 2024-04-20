42372f349cb2d9b0b07ca590c7f945ef7b11d560
D3centX <109889758+Diegoson@users.noreply.github.com>
2024-04-20 07:21:12 +0200

    vorterx

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -1,29 +1,32 @@
+const { messages, getMessage } = require('../lib/messages');
+const getLyrics = require("@fantox01/lyrics-scraper");
 
+exports.default = {
+  name: 'lyrics',
+  category: 'search',
+  carryOut: async (nova, m, { react, match }) => {
+   
+ if (!match) {
+      await react('🐊');
+      return m.reply(getMessage('title_name'));
+    }
+
+    try {
+      const { artist: a, album: ab, release_date: rd, thumbnail: th, lyrics: l } = await getLyrics(match);
+
+      const lyric_table = `
+        \n*Title*: ${match}` +
+        `\n*Artist*: ${a}` +
+        `\n*Released*: ${rd}` +
+        `\n*Album*: ${ab}` +
+        `\n\n*Lyrics*:\n${l}
+      `.trim();
+
+      nova.sendMessage(m.chat, { image: { url: th }, text: lyric_table}, {quoted: m });
+    } catch (error) {
+      console.error(error);
+      m.reply(error);
+    }
+  }
+};
             
-          if (!text) return reply(`Command usage: ${prefix}lyrics <song title>`);
-        
-          reply(mess.waiting);
-        
-          const getLyrics = require("@fantox01/lyrics-scraper");
-        
-          try {
-            const data = await getLyrics(text);
-        
-            const message = `
-        *Title:* ${text}
-        *Artist:* ${data.artist}
-        *Album:* ${data.album}
-        *Release Date:* ${data.release_date}
-        *URL:* ${data.url}
-        *Thumbnail:* ${data.thumbnail}
-        
-        *Lyrics:*\n${data.lyrics}
-            `.trim();
-        
-            Maria.sendMessage(from, { text: message, quoted: m });
-          } catch (error) {
-            console.error('Error fetching lyrics:', error);
-            const errorMessage = 'Failed to fetch lyrics. Please try again later.';
-            Maria.sendMessage(from, { text: errorMessage, quoted: m });
-          }
-          break;
