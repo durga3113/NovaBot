cb6e43679b04f97a134c5d545d405670e80505a9
D3centX <109889758+Diegoson@users.noreply.github.com>
2024-04-27 20:41:54 +0200

    vorterx

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,22 +1,43 @@
+const config = require('../config');
+
 exports.default = {
   name: 'sticker',
   category: 'converter',
-  carryOut: async (nova,m,{react,args}) => {
+  carryOut: async (nova, m, { react, args, mime }) => {
 
-  if (!(message.reply_message.video || message.reply_message.image))
-      return await message.reply("*_Reply to photo or video!_*");
-    let buff = await m.quoted.download();
-    await nova.sendMessage(m.chat,{
-      buff,
-      { packname: config.STICKER_DATA.split(";")[0], contextInfo: { externalAdReply: {
-title: "",
-body: "",
-sourceUrl: "",
-mediaUrl: "",
-mediaType: 1,
-showAdAttribution: true,
-thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg" }} },
-      "sticker"
-    );
+    if (!m.quoted) {
+      return m.reply("Please reply to a photo or video to convert it into a sticker.");
+    }
+
+    const mime = m.quoted.mimetype;
+    if (!(/image|video/.test(mime))) {
+      return m.reply("*_Please reply to a photo or video._*");
+    }
+
+    try {
+      let mediaBuffer = await m.quoted.download();
+      await nova.sendMessage(m.chat, {
+        sticker: mediaBuffer,
+        contextInfo: {
+          externalAdReply: {
+            title: "",
+            body: "",
+            sourceUrl: "",
+            mediaUrl: "",
+            mediaType: 1,
+            showAdAttribution: true,
+            thumbnailUrl: "https://i.imgur.com/Ou56ggv.jpeg"
+          }
+        }
+      }, {
+        packname: config.PACKNAME.split(";")[0],
+        author: "" 
+      });
+
+    } catch (error) {
+      console.error("*_Error_*:", error);
+      m.reply("*_Error_*");
+    }
   }
-);
+};
+    
