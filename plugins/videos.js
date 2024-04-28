8280e9b37047b998b6590adf0b22ce65851c90a6
D3centX <109889758+Diegoson@users.noreply.github.com>
2024-04-28 17:03:53 +0200

    vorterx

---
+++ plugins/videos.js
@@ -0,0 +1,66 @@
+export.default = {
+name: 'video',
+category: 'download',
+carryOut: async (nova, m, { react, args }) => {
+if (!args) {
+await react('🈵');
+return m.reply('Provide a video name or URL please');
+}
+
+const url = args[0];
+const videoTitle = args[1];
+
+const ytdl = require('ytdl-core');
+const yts = require('yt-search');
+
+const ytRegex = /^https?://(?:www.|m.)?(link unavailable);
+
+if (!url.match(ytRegex)) {
+return m.reply('Invalid YouTube URL');
+}
+
+try {
+let info;
+
+if (url.startsWith('https://') || url.startsWith('http://')) {
+info = await ytdl.getInfo(url);
+} else {
+const searchResult = await yts.search(url);
+info = await ytdl.getInfo(searchResult.videos[0].url);
+}
+
+const title = info.title;
+const size = info.length;
+const quality = info.formats[0].qualityLabel;
+
+m.reply(`*_Downloading_*: ${title}...`);
+
+const stream = ytdl(info.formats[0].url, {
+filter: 'audioandvideo',
+quality: 'highest',
+});
+
+const writer = nova.fs.createWriteStream(`${title}.${info.formats[0].ext}`);
+
+stream.pipe(writer);
+
+writer.on('finish', async () => {
+await nova.sendMessage(m.chat, {
+video: {
+file: nova.fs.readFileSync(`${title}.${info.formats[0].ext}`),
+filename: `${title}.${info.formats[0].ext}`,
+},
+caption: `*Title:${title}*\n*Size: ${size} seconds*\n*Quality: ${quality}*`,
+externalAdReply: {
+title: 'NovaBot',
+body: 'vn_ext',
+thumbnail: info.thumbnail,
+},
+  });
+});
+} catch (err) {
+m.reply(`Error: ${err.message}`);
+  }
+ }
+}
+                 
